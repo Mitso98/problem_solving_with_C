6a05874ffec2b5ff89e4155077b7166f03280bd2
Mosatfa Ahmed <mostafa.ahmed19293949@gmail.com>
2022-11-04 20:24:24 +0200

    scan input from user.

--- lab4/lab_q7/main.c
+++ lab4/lab_q7/main.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 100
 //6) C Program to Find the Length of a String without Using strlen()
-int str_cpy(const char* target);
+int str_len(const char* target);
 int main()
 {
-    char str[50] = "Mostafa";
+    char str[SIZE];
+    int counter = 0;
+    char input = 'i'; //initialize a dummy value
+
+    printf("Enter a value: ");
+    while (counter < SIZE && 10 != input) // 10 is ASCI for ENTER key
+    {
+        scanf("%c", &input);
+        // will count any thing except enter
+        if (10 != input)
+        {
+            str[counter] = input;
+            counter++;
+        }
+    }
     printf(">>> %d", str_len(str));
     return 0;
 }
-int str_len(const char* target){
+int str_len(const char* target)
+{
+    //loops until it finds the terminator
     int counter = 0;
-    while ('\0' != target[counter]){
+    while ('\0' != target[counter])
+    {
         counter++;
     }
     return counter;
