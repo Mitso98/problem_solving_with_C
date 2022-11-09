ce369c3ad5473e180a1a1b97c29ae18024fedc94
Mosatfa Ahmed <mostafa.ahmed19293949@gmail.com>
2022-11-09 21:55:39 +0200

    design change

--- lab7/lab7_text_editor/main.c
+++ lab7/lab7_text_editor/main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <dos.h>
 #include <dir.h>
-#include<windows.h>
-
+#include <windows.h>
+// TODO fix max column and backspace bug
 typedef struct node
 {
     char val;
-    struct node * next;
+    int col_pos;
+    int row_pos;
+    struct node *next;
 } Node;
 
 typedef struct column_size
@@ -15,15 +17,15 @@ typedef struct column_size
     // to store how many columns are at each row
     int row_num;
     int size;
-    struct column_size * next;
+    struct column_size *next;
 } Col_size;
 
-void push(Node * head, int val, int * max_col, int * max_row, int *col, int * row, Col_size * head_col_count);
-void print_char(Node * head, int col, int row, int is_all);
-void print_all(Node * head);
-void control_extended_keys(int input, int *col, int *row, int max_col, int max_row, Col_size * head_col_size);
-void remove_node(int * col, int * row, int * max_col, int * max_row, Node * head);
-void gotoxy(int x,int y);
+void push(Node *head, int val, int *max_col, int *max_row, int *col, int *row);
+void print_char(Node *head);
+// void print_all(Node *head);
+void control_extended_keys(int input, int *col, int *row, int max_col, int max_row);
+void remove_node(int *col, int *row, int *max_col, int *max_row, Node *head);
+void gotoxy(int x, int y);
 
 int main()
 {
@@ -33,9 +35,7 @@ int main()
     int max_col = 0;
     int max_row = 0;
 
-    Col_size * head_col_count = (Col_size *) malloc(sizeof(Col_size));
-    head_col_count->next = NULL;
-    Node * head = (Node *) malloc(sizeof(Node));
+    Node *head = (Node *)malloc(sizeof(Node));
     head->next = NULL;
 
     do
@@ -44,122 +44,119 @@ int main()
         if (224 == input)
         {
             input = getch();
-            control_extended_keys(input, &col, &row, max_col, max_row, head_col_count);
+            control_extended_keys(input, &col, &row, max_col, max_row);
         }
         else
         {
-            if (8 == input)
+            // 8 is the back space
+            if (8 == input && 0 <= col)
             {
                 remove_node(&col, &row, &max_col, &max_row, head);
-                print_char(head, col, row, 1);
+                print_char(head);
             }
             else
             {
-
-                push(head, input, &max_col, &max_row, &col, &row, head_col_count);
-                print_char(head, col, row, 0);
+                push(head, input, &max_col, &max_row, &col, &row);
+                print_char(head);
             }
         }
-
-        gotoxy(50,20);
+        gotoxy(50, 20);
         printf("col: %d row: %d\n", col, row);
         printf("max_col: %d max_row: %d\n", max_col, max_row);
-        gotoxy(col,row);
-    }
-    while(27 != input);
+        gotoxy(col, row);
+    } while (27 != input);
 
-    print_all(head);
+    print_char(head);
     return 0;
 }
-void push(Node * head, int val, int * max_col, int * max_row, int *col, int * row, Col_size * head_col_count)
+void push(Node *head, int val, int *max_col, int *max_row, int *col, int *row)
 {
+    Node *current = head;
+    // push to the end of the list.
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    // instructions if new line is entered
     if (13 == val)
     {
-        Col_size * current_col_count = head_col_count;
-        while (current_col_count->row_num != *row && current_col_count->next != NULL)
-        {
-            current_col_count = current_col_count->next;
-        }
-        current_col_count->next = (Col_size *) malloc(sizeof(Col_size));
-        current_col_count->next->size = *col;
-        current_col_count->next->row_num = *row;
-        current_col_count->next->next= NULL;
-
+        current->next = (Node *)malloc(sizeof(Node));
+        current->next->val = '\n';
         *max_row += 1;
         *row += 1;
+        current->next->row_pos = *max_row;
         *col = 0;
-        val = '\n';
+        current->next->col_pos = *col;
+        current->next->next = NULL;
     }
     else
     {
+        // instructions for any other value
+        current->next = (Node *)malloc(sizeof(Node));
+        current->next->val = val;
+        current->next->row_pos = *max_row;
         *col += 1;
+        current->next->col_pos = *col;
+        current->next->next = NULL;
     }
-    Node * current = head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-    // now we can add a new variable
-    current->next = (Node *) malloc(sizeof(Node));
-    current->next->val = val;
-    current->next->next = NULL;
-
-    *max_col = *col > *max_col? *col: *max_col;
+    // increase max_col only if col exceded it
+    *max_col = *col > *max_col ? *col : *max_col;
+    gotoxy(*col, *row);
 }
-void print_char(Node * head, int col, int row, int is_all)
+void print_char(Node *head)
 {
-    Node * current = head;
+    Node *current = head;
     system("cls");
     while (current->next != NULL)
     {
         current = current->next;
         printf("%c", current->val);
     }
-
 }
-void print_all(Node * head)
+/*
+
+void print_all(Node *head)
 {
     system("cls");
-    Node * current = head;
+    Node *current = head;
     while (current->next != NULL)
     {
-
-
         current = current->next;
         printf("%c", current->val);
     }
-
 }
-int get_col_size(int row, Col_size * head_col_size)
+*/
+/*
+
+int get_col_size(int row, Node *head)
 {
-    Col_size * current = head_col_size;
-    while(current->row_num != row && current->next != NULL)
+    Col_size *current = head_col_size;
+    while (current->row_num != row && current->next != NULL)
     {
         current = current->next;
     }
     return current->size;
 }
-void control_extended_keys(int input, int *col, int *row, int max_col, int max_row, Col_size * head_col_size)
+*/
+void control_extended_keys(int input, int *col, int *row, int max_col, int max_row)
 {
     // 72 up arrow
     if (72 == input && 0 < *row)
     {
         *row -= 1;
-        *col = get_col_size(*row, head_col_size);
         gotoxy(*col, *row);
     }
-    else if (80 == input && *row < max_row)// 80 down arrow
+    else if (80 == input && *row < max_row) // 80 down arrow
     {
         *row += 1;
-        *col = get_col_size(*row, head_col_size);
         gotoxy(*col, *row);
     }
-    else if (77 == input && *col < max_col)// 77 right arrow
+    else if (77 == input && *col < max_col) // 77 right arrow
     {
         *col += 1;
         gotoxy(*col, *row);
     }
-    else if (75 == input && 0 < *col)     // 75 left arrow
+    else if (75 == input && 0 < *col) // 75 left arrow
     {
         *col -= 1;
         gotoxy(*col, *row);
@@ -171,7 +168,7 @@ void control_extended_keys(int input, int *col, int *row, int max_col, int max_r
         *row = 0;
         gotoxy(*col, *row);
     }
-    else if(82 == input)
+    else if (82 == input)
     {
         // 82 == insert key go last element
         *col = max_col;
@@ -179,13 +176,17 @@ void control_extended_keys(int input, int *col, int *row, int max_col, int max_r
         gotoxy(*col, *row);
     }
 }
-void remove_node(int * col, int * row, int * max_col, int * max_row, Node * head)
+void remove_node(int *col, int *row, int *max_col, int *max_row, Node *head)
 {
+    if (0 == *row && 0 == *col)
+    {
+        return;
+    }
     // this function will remove last element
-    Node * end = head;
-    Node * prev = NULL;
-    Node * prev_prev = prev;
-    while(end->next != NULL)
+    Node *end = head;
+    Node *prev = NULL;
+    Node *prev_prev = prev;
+    while (end->next != NULL)
     {
         prev_prev = prev;
         prev = end;
@@ -194,27 +195,29 @@ void remove_node(int * col, int * row, int * max_col, int * max_row, Node * head
 
     prev->next = NULL;
 
-    *col -= 1;
     free(end);
     // to remove the character and th new line to move the cursor up
-    if(prev->val == '\n')
+    *max_col = *max_col == *col ? *col - 1 : *max_col;
+    if (prev->val == '\n')
     {
         prev_prev->next = NULL;
         free(prev);
         *row -= 1;
         *max_row -= 1;
+        *col = prev_prev->col_pos;
+    }
+    else
+    {
+
+        *col -= 1;
     }
     gotoxy(*col, *row);
 }
-void gotoxy(int x,int y)
+
+void gotoxy(int x, int y)
 {
-    COORD coord= {0,0};
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+    COORD coord = {0, 0};
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
-
-
-
-
-
